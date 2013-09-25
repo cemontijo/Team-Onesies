3113fb2c1f6b9467a67057eb988175c399463474
aschweighofer <achim.schweighofer@gmail.com>
2013-09-25 17:34:54 -0600

    Monitor first design

---
+++ Senqual/app/presenters/MonitorPresenter.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Monitor presenter.
+ */
+class MonitorPresenter extends BasePresenter
+{
+
+	public function renderDefault()
+	{
+		$this->template->anyVariable = 'any value';
+	}
+
+}
