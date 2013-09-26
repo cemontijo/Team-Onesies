45b58b70bacdea47cedf3f03c2fc847c59cbcae2
cemontijo <cemontijo@yahoo.com>
2013-09-25 19:26:37 -0600

    Update storeSerializedObjs.php

--- Senqual/Classes/storeSerializedObjs.php
+++ Senqual/Classes/storeSerializedObjs.php
@@ -6,6 +6,7 @@
 $conn = odbc_connect("webdb", "php", "chicken");
 $stmt = odbc_prepare($conn, "UPDATE sessions SET data = ? WHERE id = ?");
 $sqldata = array (serialize($session_data), $_SERVER['PHP_AUTH_USER']);
+
 if (!odbc_execute($stmt, $sqldata)) {
     $stmt = odbc_prepare($conn,
      "INSERT INTO sessions (id, data) VALUES(?, ?)");
