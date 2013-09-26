c300ee76209e3a1ae667e6240858a06208e77cc2
hdramirez <@rebeh140>
2013-09-26 15:14:16 -0600

    Profile Page

--- Senqual/app/presenters/ProfilePresenter.php
+++ Senqual/app/presenters/ProfilePresenter.php
@@ -3,12 +3,19 @@
 /**
  * Profile presenter.
  */
-class ProfilePresenter
+class ProfilePresenter extends BasePresenter
 {
+	/** @var Nette\Database\Connection */
+	private $database;
+	
+	public function __construct(Nette\Database\Connection $database)
+	{
+		$this->database = $database;
+	}
 
 	public function renderDefault()
 	{
-		$this->template->anyVariable = 'any value';
+		$this->template->monitors = $this->database->table('monitors');
 	}
 
 }
