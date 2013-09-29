6ec95ae50105ba0d61bb9702a3e85047a7f2be11
hdramirez <@rebeh140>
2013-09-29 00:25:42 -0600

    Updated files from Profile
    No functionality yet.

--- Senqual/app/presenters/ProfilePresenter.php
+++ Senqual/app/presenters/ProfilePresenter.php
@@ -7,6 +7,12 @@ class ProfilePresenter extends BasePresenter
 {
 	/** @var Nette\Database\Connection */
 	private $database;
+	private $name;
+	private $title;
+	private $email;
+	private $password;
+	private $phone;
+	
 	
 	public function __construct(Nette\Database\Connection $database)
 	{
@@ -15,7 +21,33 @@ class ProfilePresenter extends BasePresenter
 
 	public function renderDefault()
 	{
-		$this->template->users = $this->database->table('user_profile');
+		$this->template->profile = $this->database->table('user_profile');
+		$test=$this->database->table('user_profile');
+		
+		
+	}
+	public function setName($name)
+	{
+		
+	}
+	public function editValues()
+	{
+		
+	}
+	public function handleUpdateData()
+	{
+		$id = $this->getParam('id');
+		$fields = (array) $this->getParam('user_profile');
+		$tableName = $this->getParam('user_profile');
+		$row = $this->database->update($tableName, $fields, $id);
+		
+		
+		
+		//$control->getComponent('name')->render();
+		//echo $contol;
+
 	}
 
 }
+
+
