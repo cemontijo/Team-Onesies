2ad6999b78a70137a2d2d46b57a7b0b86bc5f258
cemontijo <cemontijo@yahoo.com>
2013-11-25 21:29:38 -0700

    updated the database

--- DB_backups/senqual.sql
+++ DB_backups/senqual.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 30, 2013 at 07:45 AM
+-- Generation Time: Nov 26, 2013 at 05:12 AM
 -- Server version: 5.5.32
 -- PHP Version: 5.4.19
 
@@ -25,12 +25,41 @@ USE `senqual`;
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `fields`
+--
+
+CREATE TABLE IF NOT EXISTS `fields` (
+  `field_name` char(255) NOT NULL,
+  `unit_measure` char(255) NOT NULL,
+  PRIMARY KEY (`field_name`,`unit_measure`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `monitored_readings`
+--
+
+CREATE TABLE IF NOT EXISTS `monitored_readings` (
+  `reading` text NOT NULL,
+  `date_monitored` date NOT NULL,
+  `anomaly_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `rule_id` int(11) NOT NULL,
+  `monitor_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `monitors`
 --
 
 CREATE TABLE IF NOT EXISTS `monitors` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
+  `date_modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `date_created` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `creator` text NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
 
@@ -38,10 +67,10 @@ CREATE TABLE IF NOT EXISTS `monitors` (
 -- Dumping data for table `monitors`
 --
 
-INSERT INTO `monitors` (`id`, `name`) VALUES
-(1, 'TestMonitor13'),
-(2, 'Test-#+´´ symbols Monitor 2'),
-(3, 'jkjk1');
+INSERT INTO `monitors` (`id`, `name`, `date_modified`, `date_created`, `creator`) VALUES
+(1, 'TestMonitor13', '2013-11-26 03:55:22', '0000-00-00 00:00:00', 'cemontijo@live.com'),
+(2, 'Test-#+´´ symbols Monitor 2', '2013-11-26 03:55:55', '0000-00-00 00:00:00', 'cemontijo@live.com'),
+(3, 'jkjk1', '2013-11-26 03:55:55', '0000-00-00 00:00:00', 'cemontijo@live.com');
 
 -- --------------------------------------------------------
 
@@ -96,6 +125,26 @@ CREATE TABLE IF NOT EXISTS `rules` (
 INSERT INTO `rules` (`id`, `dnl`, `rule_string`, `creator`, `date_modified`, `date_created`, `class`, `scope_statement1`, `scope_statement2`, `pattern_premise`, `pattern_statement`, `scope_string`, `pattern_string`) VALUES
 ('rule1', 'Testdnl1', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer aliquam leo lectus, eget placerat risus vehicula sit amet. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus cursus elit nec magna commodo varius. Nam tincidunt nunc scelerisque mi tincidunt facilisis. Donec ullamcorper quam id quam sodales sollicitudin. Fusce quis leo sit amet dui tempor viverra. Fusce molestie erat quis turpis elementum accumsan. Proin ut eleifend mi. Fusce sollicitudin ante orci, in fringilla lorem malesuada nec. Sed quis lectus in enim elementum consectetur.', 'aschweighofer@miners.utep.edu', '2013-09-25 23:17:19', '2013-09-25 23:16:03', '0', '', '', '', '', '', 0),
 ('rule2', 'TestDnl2', 't amet dui tempor viverra. Fusce molestie erat quis turpis elementum accumsan. Proin ut eleifend mi. Fusce sollicitudin ante orci, in fringilla lorem malesuada nec. Sed quis lectus in en', 'newminer@utep.edu', '2013-09-25 23:17:56', '2013-09-25 23:17:56', '0', '', '', '', '', '', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sensors`
+--
+
+CREATE TABLE IF NOT EXISTS `sensors` (
+  `identifier` char(255) NOT NULL,
+  `serial_number` text NOT NULL,
+  `type` text NOT NULL,
+  `location` text NOT NULL,
+  `latitude` text NOT NULL,
+  `longitude` text NOT NULL,
+  `accuracy` text NOT NULL,
+  `date_modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `date_created` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `creator` text NOT NULL,
+  PRIMARY KEY (`identifier`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -159,6 +208,12 @@ INSERT INTO `user_profile` (`id`, `email`, `name`, `title`, `affiliation`, `pass
 ALTER TABLE `monitor_rules`
   ADD CONSTRAINT `monitor_rules_ibfk_1` FOREIGN KEY (`monitor_id`) REFERENCES `monitors` (`id`),
   ADD CONSTRAINT `monitor_rules_ibfk_2` FOREIGN KEY (`rule_id`) REFERENCES `rules` (`id`);
+
+--
+-- Constraints for table `sensors`
+--
+ALTER TABLE `sensors`
+  ADD CONSTRAINT `sensors_ibfk1` FOREIGN KEY (`identifier`) REFERENCES `fields` (`field_name`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
