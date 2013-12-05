a34aca404de5079add9655c51059fef33eff091d
hdramirez <@rebeh140>
2013-12-04 20:00:35 -0700

    Senqual database most recent updated
    Updated senqual database with hard coded rules and data

--- DB_backups/senqual.sql
+++ DB_backups/senqual.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 27, 2013 at 10:38 AM
+-- Generation Time: Dec 05, 2013 at 03:54 AM
 -- Server version: 5.5.27
 -- PHP Version: 5.4.7
 
@@ -27,108 +27,110 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `data` (
-  `date` text NOT NULL,
-  `time` text NOT NULL,
-  `reading` int(11) NOT NULL
+  `date` char(255) NOT NULL,
+  `time` char(255) NOT NULL,
+  `reading` int(11) NOT NULL,
+  `unitMeasure` char(255) NOT NULL,
+  `type` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `data`
 --
 
-INSERT INTO `data` (`date`, `time`, `reading`) VALUES
-('11-15-2013', '1:00', 100),
-('11-15-2013', '1:15', 110),
-('11-15-2013', '1:30', 95),
-('11-15-2013', '1:45', 99),
-('11-15-2013', '2:00', 99),
-('11-15-2013', '2:15', 95),
-('11-15-2013', '2:30', 105),
-('11-15-2013', '2:45', 100),
-('11-15-2013', '3:00', 90),
-('11-15-2013', '3:15', 98),
-('11-15-2013', '3:30', 110),
-('11-15-2013', '3:45', 95),
-('11-15-2013', '4:00', 115),
-('11-15-2013', '4:15', 105),
-('11-15-2013', '4:30', 90),
-('11-15-2013', '4:45', 100),
-('11-15-2013', '5:00', 95),
-('11-15-2013', '5:15', 110),
-('11-15-2013', '5:30', 105),
-('11-15-2013', '5:45', 110),
-('11-15-2013', '6:00', 90),
-('11-15-2013', '6:15', 99),
-('11-15-2013', '6:30', 95),
-('11-15-2013', '6:45', 110),
-('11-15-2013', '7:00', 100),
-('11-15-2013', '7:15', 97),
-('11-15-2013', '7:30', 96),
-('11-15-2013', '7:45', 100),
-('11-15-2013', '8:00', 103),
-('11-15-2013', '8:15', 99),
-('11-15-2013', '8:30', 96),
-('11-15-2013', '8:45', 102),
-('11-15-2013', '9:00', 110),
-('11-15-2013', '9:15', 100),
-('11-15-2013', '9:30', 95),
-('11-15-2013', '9:45', 110),
-('11-15-2013', '10:00', 98),
-('11-15-2013', '10:15', 90),
-('11-15-2013', '10:30', 100),
-('11-15-2013', '10:45', 110),
-('11-15-2013', '11:00', 95),
-('11-15-2013', '11:15', 99),
-('11-15-2013', '6:30', 94),
-('11-15-2013', '11:45', 110),
-('11-15-2013', '12:00', 100),
-('11-15-2013', '12:15', 94),
-('11-15-2013', '12:30', 99),
-('11-15-2013', '12:45', 110),
-('11-15-2013', '13:00', 100),
-('11-15-2013', '13:15', 95),
-('11-15-2013', '13:30', 99),
-('11-15-2013', '13:45', 100),
-('11-15-2013', '14:00', 99),
-('11-15-2013', '14:15', 96),
-('11-15-2013', '14:30', 99),
-('11-15-2013', '14:45', 100),
-('11-15-2013', '15:00', 110),
-('11-15-2013', '15:15', 100),
-('11-15-2013', '15:30', 95),
-('11-15-2013', '15:45', 110),
-('11-15-2013', '16:00', 100),
-('11-15-2013', '16:15', 95),
-('11-15-2013', '16:30', 99),
-('11-15-2013', '16:45', 110),
-('11-15-2013', '17:00', 100),
-('11-15-2013', '17:15', 95),
-('11-15-2013', '17:30', 99),
-('11-15-2013', '17:45', 108),
-('11-15-2013', '18:00', 100),
-('11-15-2013', '18:15', 96),
-('11-15-2013', '18:30', 99),
-('11-15-2013', '18:45', 110),
-('11-15-2013', '19:00', 100),
-('11-15-2013', '19:15', 95),
-('11-15-2013', '19:30', 100),
-('11-15-2013', '19:45', 110),
-('11-15-2013', '20:00', 100),
-('11-15-2013', '20:15', 95),
-('11-15-2013', '20:30', 99),
-('11-15-2013', '20:45', 110),
-('11-15-2013', '21:00', 100),
-('11-15-2013', '21:15', 90),
-('11-15-2013', '21:30', 95),
-('11-15-2013', '21:45', 110),
-('11-15-2013', '22:00', 100),
-('11-15-2013', '22:15', 93),
-('11-15-2013', '22:30', 96),
-('11-15-2013', '22:45', 115),
-('11-15-2013', '23:00', 110),
-('11-15-2013', '23:15', 98),
-('11-15-2013', '23:30', 105),
-('11-15-2013', '23:45', 110);
+INSERT INTO `data` (`date`, `time`, `reading`, `unitMeasure`, `type`) VALUES
+('11-15-2013', '1:00', 100, 'fahrenheit', 'temperature'),
+('11-15-2013', '1:15', 110, 'fahrenheit', 'temperature'),
+('11-15-2013', '1:30', 95, 'fahrenheit', 'temperature'),
+('11-15-2013', '1:45', 99, 'fahrenheit', 'temperature'),
+('11-15-2013', '2:00', 99, 'fahrenheit', 'temperature'),
+('11-15-2013', '2:15', 95, 'fahrenheit', 'temperature'),
+('11-15-2013', '2:30', 105, 'fahrenheit', 'temperature'),
+('11-15-2013', '2:45', 100, 'fahrenheit', 'temperature'),
+('11-15-2013', '3:00', 90, 'fahrenheit', 'temperature'),
+('11-15-2013', '3:15', 98, 'fahrenheit', 'temperature'),
+('11-15-2013', '3:30', 110, 'fahrenheit', 'temperature'),
+('11-15-2013', '3:45', 95, 'fahrenheit', 'temperature'),
+('11-15-2013', '4:00', 115, 'fahrenheit', 'temperature'),
+('11-15-2013', '4:15', 105, 'fahrenheit', 'temperature'),
+('11-15-2013', '4:30', 90, 'fahrenheit', 'temperature'),
+('11-15-2013', '4:45', 100, 'fahrenheit', 'temperature'),
+('11-15-2013', '5:00', 95, 'fahrenheit', 'temperature'),
+('11-15-2013', '5:15', 110, 'fahrenheit', 'temperature'),
+('11-15-2013', '5:30', 105, 'fahrenheit', 'temperature'),
+('11-15-2013', '5:45', 110, 'fahrenheit', 'temperature'),
+('11-15-2013', '6:00', 90, 'fahrenheit', 'temperature'),
+('11-15-2013', '6:15', 99, 'fahrenheit', 'temperature'),
+('11-15-2013', '6:30', 95, 'fahrenheit', 'temperature'),
+('11-15-2013', '6:45', 110, 'fahrenheit', 'temperature'),
+('11-15-2013', '7:00', 100, 'fahrenheit', 'temperature'),
+('11-15-2013', '7:15', 97, 'fahrenheit', 'temperature'),
+('11-15-2013', '7:30', 96, 'fahrenheit', 'temperature'),
+('11-15-2013', '7:45', 100, 'fahrenheit', 'temperature'),
+('11-15-2013', '8:00', 103, 'fahrenheit', 'temperature'),
+('11-15-2013', '8:15', 99, 'fahrenheit', 'temperature'),
+('11-15-2013', '8:30', 96, 'fahrenheit', 'temperature'),
+('11-15-2013', '8:45', 102, 'fahrenheit', 'temperature'),
+('11-15-2013', '9:00', 110, 'fahrenheit', 'temperature'),
+('11-15-2013', '9:15', 100, 'fahrenheit', 'temperature'),
+('11-15-2013', '9:30', 95, 'fahrenheit', 'temperature'),
+('11-15-2013', '9:45', 110, 'fahrenheit', 'temperature'),
+('11-15-2013', '10:00', 98, 'fahrenheit', 'temperature'),
+('11-15-2013', '10:15', 90, 'fahrenheit', 'temperature'),
+('11-15-2013', '10:30', 100, 'fahrenheit', 'temperature'),
+('11-15-2013', '10:45', 110, 'fahrenheit', 'temperature'),
+('11-15-2013', '11:00', 95, 'fahrenheit', 'temperature'),
+('11-15-2013', '11:15', 99, 'fahrenheit', 'temperature'),
+('11-15-2013', '6:30', 94, 'fahrenheit', 'temperature'),
+('11-15-2013', '11:45', 110, 'fahrenheit', 'temperature'),
+('11-15-2013', '12:00', 100, 'fahrenheit', 'temperature'),
+('11-15-2013', '12:15', 94, 'fahrenheit', 'temperature'),
+('11-15-2013', '12:30', 99, 'fahrenheit', 'temperature'),
+('11-15-2013', '12:45', 110, 'fahrenheit', 'temperature'),
+('11-15-2013', '13:00', 100, 'fahrenheit', 'temperature'),
+('11-15-2013', '13:15', 95, 'fahrenheit', 'temperature'),
+('11-15-2013', '13:30', 99, 'fahrenheit', 'temperature'),
+('11-15-2013', '13:45', 100, 'fahrenheit', 'temperature'),
+('11-15-2013', '14:00', 99, 'fahrenheit', 'temperature'),
+('11-15-2013', '14:15', 96, 'fahrenheit', 'temperature'),
+('11-15-2013', '14:30', 99, 'fahrenheit', 'temperature'),
+('11-15-2013', '14:45', 100, 'fahrenheit', 'temperature'),
+('11-15-2013', '15:00', 110, 'fahrenheit', 'temperature'),
+('11-15-2013', '15:15', 100, 'fahrenheit', 'temperature'),
+('11-15-2013', '15:30', 95, 'fahrenheit', 'temperature'),
+('11-15-2013', '15:45', 110, 'fahrenheit', 'temperature'),
+('11-15-2013', '16:00', 100, 'fahrenheit', 'temperature'),
+('11-15-2013', '16:15', 95, 'fahrenheit', 'temperature'),
+('11-15-2013', '16:30', 99, 'fahrenheit', 'temperature'),
+('11-15-2013', '16:45', 110, 'fahrenheit', 'temperature'),
+('11-15-2013', '17:00', 100, 'fahrenheit', 'temperature'),
+('11-15-2013', '17:15', 95, 'fahrenheit', 'temperature'),
+('11-15-2013', '17:30', 99, 'fahrenheit', 'temperature'),
+('11-15-2013', '17:45', 108, 'fahrenheit', 'temperature'),
+('11-15-2013', '18:00', 100, 'fahrenheit', 'temperature'),
+('11-15-2013', '18:15', 96, 'fahrenheit', 'temperature'),
+('11-15-2013', '18:30', 99, 'fahrenheit', 'temperature'),
+('11-15-2013', '18:45', 110, 'fahrenheit', 'temperature'),
+('11-15-2013', '19:00', 100, 'fahrenheit', 'temperature'),
+('11-15-2013', '19:15', 95, 'fahrenheit', 'temperature'),
+('11-15-2013', '19:30', 100, 'fahrenheit', 'temperature'),
+('11-15-2013', '19:45', 110, 'fahrenheit', 'temperature'),
+('11-15-2013', '20:00', 100, 'fahrenheit', 'temperature'),
+('11-15-2013', '20:15', 95, 'fahrenheit', 'temperature'),
+('11-15-2013', '20:30', 99, 'fahrenheit', 'temperature'),
+('11-15-2013', '20:45', 110, 'fahrenheit', 'temperature'),
+('11-15-2013', '21:00', 100, 'fahrenheit', 'temperature'),
+('11-15-2013', '21:15', 90, 'fahrenheit', 'temperature'),
+('11-15-2013', '21:30', 95, 'fahrenheit', 'temperature'),
+('11-15-2013', '21:45', 110, 'fahrenheit', 'temperature'),
+('11-15-2013', '22:00', 100, 'fahrenheit', 'temperature'),
+('11-15-2013', '22:15', 93, 'fahrenheit', 'temperature'),
+('11-15-2013', '22:30', 96, 'fahrenheit', 'temperature'),
+('11-15-2013', '22:45', 115, 'fahrenheit', 'temperature'),
+('11-15-2013', '23:00', 110, 'fahrenheit', 'temperature'),
+('11-15-2013', '23:15', 98, 'fahrenheit', 'temperature'),
+('11-15-2013', '23:30', 105, 'fahrenheit', 'temperature'),
+('11-15-2013', '23:45', 110, 'fahrenheit', 'temperature');
 
 -- --------------------------------------------------------
 
@@ -199,18 +201,19 @@ INSERT INTO `monitor_rules` (`monitor_id`, `rule_id`) VALUES
 
 CREATE TABLE IF NOT EXISTS `rules` (
   `id` varchar(255) NOT NULL,
-  `dnl` text NOT NULL,
-  `rule_string` text NOT NULL,
+  `dnl` varchar(255) NOT NULL,
+  `rule_string` varchar(255) NOT NULL,
   `creator` varchar(255) NOT NULL,
   `date_modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `date_created` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `class` text NOT NULL,
-  `scope_statement1` text,
-  `scope_statement2` text,
-  `pattern_premise` text,
-  `pattern_statement` text,
-  `scope_string` text,
-  `pattern_string` text,
+  `scope_class` varchar(255) NOT NULL,
+  `pattern_class` varchar(255) NOT NULL,
+  `scope_statement1` varchar(255) DEFAULT NULL,
+  `scope_statement2` varchar(255) DEFAULT NULL,
+  `pattern_premise` varchar(255) DEFAULT NULL,
+  `pattern_statement` varchar(255) DEFAULT NULL,
+  `scope_string` varchar(255) DEFAULT NULL,
+  `pattern_string` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -218,9 +221,28 @@ CREATE TABLE IF NOT EXISTS `rules` (
 -- Dumping data for table `rules`
 --
 
-INSERT INTO `rules` (`id`, `dnl`, `rule_string`, `creator`, `date_modified`, `date_created`, `class`, `scope_statement1`, `scope_statement2`, `pattern_premise`, `pattern_statement`, `scope_string`, `pattern_string`) VALUES
-('rule1', 'Testdnl1', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer aliquam leo lectus, eget placerat risus vehicula sit amet. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus cursus elit nec magna commodo varius. Nam tincidunt nunc scelerisque mi tincidunt facilisis. Donec ullamcorper quam id quam sodales sollicitudin. Fusce quis leo sit amet dui tempor viverra. Fusce molestie erat quis turpis elementum accumsan. Proin ut eleifend mi. Fusce sollicitudin ante orci, in fringilla lorem malesuada nec. Sed quis lectus in enim elementum consectetur.', 'aschweighofer@miners.utep.edu', '2013-11-27 02:24:28', '2013-09-25 23:16:03', 'global scope', '', '', '', '', '', '0'),
-('rule2', 'TestDnl2', 't amet dui tempor viverra. Fusce molestie erat quis turpis elementum accumsan. Proin ut eleifend mi. Fusce sollicitudin ante orci, in fringilla lorem malesuada nec. Sed quis lectus in en', 'newminer@utep.edu', '2013-11-27 02:25:07', '2013-09-25 23:17:56', 'absence pattern', '', '', '', '', '', '0');
+INSERT INTO `rules` (`id`, `dnl`, `rule_string`, `creator`, `date_modified`, `date_created`, `scope_class`, `pattern_class`, `scope_statement1`, `scope_statement2`, `pattern_premise`, `pattern_statement`, `scope_string`, `pattern_string`) VALUES
+('rule1', 'For all readings, it is never the case that the statement "temperature > 115" is true within the duration.', 'global, null, null, absence, null, temperature > 115', 'aschweighofer@miners.utep.edu', '2013-12-03 04:44:08', '2013-09-25 23:16:03', 'global', 'absence', NULL, NULL, NULL, 'temperature > 115', 'For all readings, ', 'it is never the case that the statement "temperature > 115" is true within the duration.'),
+('rule2', 'TestDnl2', 'n ante orci, in fringilla lorem malesuada nec. Sed quis lectus in en', 'newminer@utep.edu', '2013-12-03 04:27:49', '2013-09-25 23:17:56', 'beforeR', 'universality', '', NULL, NULL, '', 'For all readings before the statement <scope_statement1> becomes true, ', 'it is always the case that the statement <pattern_statement> is true within the duration.'),
+('rule3', 'For all readings, it is never the case that the statement "temperature <= 93" is true within the duration.', 'global, null, null, absence, null, temperature <= 93', 'aschweighofer@miners.utep.edu', '2013-12-03 05:07:00', '2013-09-25 23:16:03', 'global', 'absence', NULL, NULL, NULL, 'temperature <= 93', 'For all readings, ', 'it is never the case that the statement "temperature <= 93" is true within the duration.');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ruletest`
+--
+
+CREATE TABLE IF NOT EXISTS `ruletest` (
+  `rule` varchar(255) NOT NULL,
+  PRIMARY KEY (`rule`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `ruletest`
+--
+
+INSERT INTO `ruletest` (`rule`) VALUES
+('temperature>110');
 
 -- --------------------------------------------------------
 
@@ -332,8 +354,7 @@ INSERT INTO `user_profile` (`id`, `email`, `name`, `title`, `affiliation`, `pass
 -- Constraints for table `monitor_rules`
 --
 ALTER TABLE `monitor_rules`
-  ADD CONSTRAINT `monitor_rules_ibfk_1` FOREIGN KEY (`monitor_id`) REFERENCES `monitors` (`id`),
-  ADD CONSTRAINT `monitor_rules_ibfk_2` FOREIGN KEY (`rule_id`) REFERENCES `rules` (`id`);
+  ADD CONSTRAINT `monitor_rules_ibfk_1` FOREIGN KEY (`monitor_id`) REFERENCES `monitors` (`id`);
 
 --
 -- Constraints for table `sensor_fields`
